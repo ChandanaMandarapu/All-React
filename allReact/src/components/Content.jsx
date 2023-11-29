6977f3ceccc79844903528cdef1be608d015b10d
ChandanaMandarapu <chandanamandarapu93@gmail.com>
2023-11-29 16:56:50 +0530

    useState practice

--- allReact/src/components/Content.jsx
+++ allReact/src/components/Content.jsx
@@ -1,13 +1,21 @@
 // import React from 'react'
+import { useState } from "react";
 
 const Content = () => {
+    const [name, setName] = useState('chandu')
+
+    const[count,setCount] = useState(0)
+
   const handleNameChange = () => {
     const names = ["krish", "karthik", "kushal"];
     const int = Math.floor(Math.random() * 3);
-    return names[int];
+    setName(names[int]);
   };
 
-  const handleClick = () => console.log("You clicked");
+  const handleClick = () => {
+    setCount(count + 1)
+    console.log(count)
+  }
 
   //   click events anonymous functions
   const handleClick2 = () => console.log("${name} was clicked");
@@ -16,13 +24,13 @@ const Content = () => {
 
   return (
     <main>
-      <p onDoubleClick={handleClick}>Hey {handleNameChange}</p>
+      <p onDoubleClick={handleClick2}>Hey {name}</p>
 
-      <button onClick={handleClick}>Click It</button>
+      <button onClick={handleNameChange}>Click It</button>
 
       {/* passing parameters anonymous functions */}
-      <button onClick={() => handleClick2("chandana")}>Click It</button>
-      
+      <button onClick={() => handleClick("chandana")}>Click It</button>
+
       {/* loging event object */}
       <button onClick={(e) => handleClick3(e)}>Click Here</button>
     </main>
